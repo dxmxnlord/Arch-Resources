1737cf7596311c383dae20cb63b3c63f64185599
Rishikesh Sriram <42875561+dxmxnlord@users.noreply.github.com>
2019-06-13 00:16:37 +0530

    Update lemonbar.sh

--- Arch-i3-files/lemonbar/lemonbar.sh
+++ Arch-i3-files/lemonbar/lemonbar.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#killall lemonbar
+#bash /home/rishikesh/.config/i3/lemon.sh | lemonbar -b -p -F#FFFFFFFF -B#AA000000 -o -5 -f "Vin Mono Pro"-8 -o 0 -f "DejaVu Sans Mono for Powerline"-13 -f "FontAwesome"-8 -g 1366x18
+
 killall lemonbar
-bash /home/rishikesh/.config/i3/lemon.sh | lemonbar -b -p -F#FFFFFFFF -B#AA000000 -o -5 -f "Vin Mono Pro"-8 -o 0 -f "DejaVu Sans Mono for Powerline"-13 -f "FontAwesome"-8 -g 1366x18
+
+bash | lemonbar -p -g x28 -F#00000000 -B#00000000 | 
+
+bash /home/rishikesh/.config/i3/lemon.sh | lemonbar -d 1 -g 1366x18+0+5 -p -F#FFFFFFFF -B#00000000 -o -5 -f "Vin Mono Pro"-8 -o 0 -f "DejaVu Sans Mono for Powerline"-13 -f "FontAwesome"-8 | bash
